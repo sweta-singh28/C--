2483ee1ab363546e824cc88ae151fda415d8caac
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-13 22:35:23 +0530

    sweta

---
+++ inc_dec_operator.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    //Pre increment operator
+    // int a = 11;
+    // cout<<++a;
+
+
+    //Post increment operator
+    // int b = 11;
+    // cout<<b++;
+    // cout<<b<<endl;
+
+
+    // int a = 6;
+    // int c = ++a +1;
+    // cout<<c<<endl;
+
+    // int b = 6;
+    // int d = b++ +1;
+    // cout<<b<<endl;
+
+
+    // int a = 6;
+    // int c = --a -1;
+    // cout<<c<<endl;
+
+    // int b = 6;
+    // int d = b-- -1;
+    // cout<<b<<endl;
+
+    int a = 5;
+    cout<<(++a) * (++a);
+    
+
+
+
+
+    return 0;
+}
