1dc7248d6f6f55b4c33338d56c322fcb326bbe08
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-13 23:10:14 +0530

    sweta

---
+++ Bitwise_operator.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // // AND operator
+    // bool a = true;
+    // bool b = false;
+    // cout<<(a & b);   //0- False
+
+    // bool a = true;
+    // bool b = true;
+    // cout<<(a & b);   //1- True
+
+
+
+
+    // OR operator
+    // bool a = true;
+    // bool b = false;
+    // cout<<(a | b);
+
+    // bool a = true;
+    // bool b = true;
+    // cout<<(a | b);
+
+    
+    // //NOT operator
+    // bool a = false;
+    // bool b = false;
+    // cout<< (~a);
+
+    //XOR operator
+    // bool a = false;
+    // bool b = false;
+    // cout<<(a^b);
+
+    bool a = false;
+    bool b = true;
+    cout<<(a^b);
+
+
+    return 0;
+}
---
+++ Break_continue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    // for(int i=0; i<n; i++){
+    //     cout<<"sweta"<<endl;
+    //     break;
+    // }
+    // cout<<"Singh";
+
+    //Continue
+    for(int i=0; i<n; i++){
+        continue;
+        cout<<"Hello"<<endl;
+    }
+    cout<<"Sweta"<<endl;
+    return 0;
+}
